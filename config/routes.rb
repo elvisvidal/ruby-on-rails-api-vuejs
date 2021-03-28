2d7c5f619c19c9d9e99392474807602318609a63
Elvis Vidal <elvis_vidal_123@hotmail.com>
2021-03-28 12:35:29 +0100

    Adding Sample Data

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,10 @@ Rails.application.routes.draw do
   end
 
   root to: "home#index"
+
+  post "refresh", controller: :refresh, action: :create
+  post "signin", controller: :signin, action: :create
+  post "signup", controller: :signup, action: :create
+  delete "signin", controller: :signin, action: :destroy
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
